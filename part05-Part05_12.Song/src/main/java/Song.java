f4828f2c88873763f834ee6930ea1c5c42f375d2
Benrichie Hewson <um2341583@gmail.com>
2022-10-15 21:19:53 +1100

    part05_12

--- part05-Part05_12.Song/src/main/java/Song.java
+++ part05-Part05_12.Song/src/main/java/Song.java
@@ -10,7 +10,29 @@ public class Song {
         this.name = name;
         this.durationInSeconds = durationInSeconds;
     }
-
+    
+    public boolean equals(Object compared){
+        // if the variables are located in the same position, they are equal
+        if(this == compared){
+            return true;
+        }
+        
+        // if the compared object is not of type , the objects are not equal
+        if(!(compared instanceof Song)){
+            return false;
+        }
+        // convert the object into a Song object
+        Song comparedSong = (Song) compared;
+        
+        if(this.artist == comparedSong.artist &&
+           this.name == comparedSong.name &&
+           this.durationInSeconds == comparedSong.durationInSeconds){
+            return true;
+        }
+        
+        return false;
+    }
+    
     @Override
     public String toString() {
         return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
