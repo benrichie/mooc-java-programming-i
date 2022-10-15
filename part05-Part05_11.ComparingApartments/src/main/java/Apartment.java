45012115da19ad156e55fad509759c1d3c4926d7
Benrichie Hewson <um2341583@gmail.com>
2022-10-15 20:45:11 +1100

    part05_11

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -10,5 +10,31 @@ public class Apartment {
         this.squares = squares;
         this.princePerSquare = pricePerSquare;
     }
+    
+    public boolean largerThan(Apartment compared){
+        if(this.squares > compared.squares){
+            return true;
+        } 
+        if(this.squares < compared.squares){
+            return false;
+        }
+        
+        return false;
+    }
+    
+    public int priceDifference(Apartment compared){
+        int difference =  (compared.princePerSquare * compared.squares) - (this.princePerSquare * this.squares);
+        if(difference < 0){
+            difference *= -1;
+        }
+        return difference;
+    }
+    
+    public boolean moreExpensiveThan(Apartment compared){
+        if(this.princePerSquare > compared.princePerSquare){
+            return true;
+        }
+        return false;
+    }
 
 }
