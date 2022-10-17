f2e81303fde4304a0e825bf371a5032fbbaa109e
Benrichie Hewson <um2341583@gmail.com>
2022-10-17 20:14:43 +1100

    part05_14

--- part05-Part05_14.Books/src/main/java/Book.java
+++ part05-Part05_14.Books/src/main/java/Book.java
@@ -16,5 +16,28 @@ public class Book {
     public int getPublicationYear() {
         return publicationYear;
     }
+    
+    @Override
+    public boolean equals(Object compared){
+        //if the variables are located in the same position, they are equal
+        if(this == compared){
+            return true;
+        }
+        //if the compared object is not of type Bird, the objects are not equal
+        if (!(compared instanceof Book)){
+            return false;
+        }
+        //convert the object to a Bird object
+        Book comparedBook = (Book) compared;
+        
+        //if the values of the object variables are equal, the objects are, too
+        if(this.name.equals(comparedBook.name)
+             && this.publicationYear == comparedBook.publicationYear){
+            return true;
+        }
+        return false;
+    }
+    
+
 
 }
