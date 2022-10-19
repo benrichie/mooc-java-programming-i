d6492d1c1a5e6f06f0016a25117ae60350fe954e
Benrichie Hewson <um2341583@gmail.com>
2022-10-19 18:39:26 +1100

    part05_16

--- part05-Part05_15.Archive/src/main/java/Main.java
+++ part05-Part05_15.Archive/src/main/java/Main.java
@@ -1,41 +1,43 @@
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Archive> items = new ArrayList<>();
-
-        while (true) {
-            System.out.println("Identifier? (empty will stop)");
-            String identifier = scanner.nextLine();
-
-            if (identifier.isEmpty()) {
-                break;
-            }
-
-            System.out.println("Name? (empty will stop)");
-            String name = scanner.nextLine();
-
-            if (name.isEmpty()) {
-                break;
-            }
-
-            Archive item = new Archive(identifier, name);
-            if (items.contains(item)) {
-                continue;
-            } else {
-                items.add(item);
-            }
-
-        }
-
-        System.out.println("==Items==");
-
-        for (Archive entry : items) {
-            System.out.println(entry.getIdentifier() + ": " + entry.getName());
-        }
-
+       Scanner scanner = new Scanner(System.in);
+       ArrayList<Archive> archive = new ArrayList<>();
+       
+       while(true){
+           
+           System.out.println("Identifier? (empty will stop)");
+           String iden = scanner.nextLine();
+           
+           if(iden.isEmpty()){
+               break;
+           }
+           
+           System.out.println("Name? (empty will stop");
+           String name = scanner.nextLine();
+           
+           if(name.isEmpty()){
+               break;
+           }
+           
+           Archive a = new Archive(iden, name);
+           if(archive.contains(a)){
+               continue;
+           }else{
+               archive.add(a);
+           }
+           
+           
+           System.out.println("==Items==");
+           //forloop to print each
+           for(Archive i: archive){
+               System.out.println(i.getIdentifier()+ ": "+ i.getName());
+           }
+       }
+       
     }
 }
