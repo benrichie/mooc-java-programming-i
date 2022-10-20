a5d40bf455e17a7bdc767ee77f16aac8913b06b2
Benrichie Hewson <um2341583@gmail.com>
2022-10-20 20:43:53 +1100

    part05_17

--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -22,7 +22,8 @@ public class Money {
     public int cents() {
         return this.cents;
     }
-
+    
+    @Override
     public String toString() {
         String zero = "";
         if (this.cents < 10) {
@@ -31,5 +32,44 @@ public class Money {
 
         return this.euros + "." + zero + this.cents + "e";
     }
+    
+    public Money plus(Money addition){
+        //create a new Money object that has the correct worth
+        //copy objects must have the same variables
+        Money newMoney = new Money(this.euros + addition.euros,  this.cents + addition.cents);
+        
+        return newMoney;
+    }
+    
+    public boolean lessThan(Money compared){
+        if(this.euros() < compared.euros()){
+            return true;
+        }
+        
+        if(this.euros() == compared.euros()){
+            if(this.cents() < compared.cents()){
+                return true;
+            }
+        }
 
+        return false;
+    }
+    
+    public Money minus(Money decreaser){
+        int euroDiff = this.euros - decreaser.euros;
+        int centDiff = this.cents - decreaser.cents;
+        int totalDiffInCents = euroDiff*100 + centDiff;
+        
+        if(totalDiffInCents <= 0){
+        return new Money(0,0);
+        
+        }else{
+        int eurosRtn = totalDiffInCents/100;
+        int centRtn = totalDiffInCents%100;
+        
+        Money newMoney = new Money(eurosRtn, centRtn);
+        return newMoney;
+        }  
+    }
+    
 }
