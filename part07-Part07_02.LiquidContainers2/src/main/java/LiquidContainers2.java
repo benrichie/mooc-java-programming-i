fc4aebee83d72f5b47d33c032f633cff863f26a1
benrichie <110895161+benrichie@users.noreply.github.com>
2022-11-23 14:06:45 +1100

    part07_02

--- part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -5,17 +5,6 @@ public class LiquidContainers2 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-
-        while (true) {
-            System.out.print("> ");
-
-            String input = scan.nextLine();
-            if (input.equals("quit")) {
-                break;
-            }
-
-        }
     }
 
 }
