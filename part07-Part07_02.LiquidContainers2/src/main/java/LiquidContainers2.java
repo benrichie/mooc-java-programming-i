9a3dda1c293fb44dbbc9e0d0455a0f6edf25151e
benrichie <110895161+benrichie@users.noreply.github.com>
2022-11-23 16:43:12 +1100

    part07_02

--- part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -4,7 +4,14 @@ import java.util.Scanner;
 public class LiquidContainers2 {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-    }
+        Scanner scanner = new Scanner(System.in);
+        Container firstContainer = new Container();
+        Container secondContainer = new Container();
+        
+        UserInterface userInterface = new UserInterface(firstContainer, secondContainer, scanner);
+        userInterface.start();
+        
+        }
+    
 
 }
