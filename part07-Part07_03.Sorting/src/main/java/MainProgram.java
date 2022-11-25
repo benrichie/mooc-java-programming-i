fcb3b6edfced2518ad44de0b66c0382c1cf7c25e
Benrichie Hewson <um2341583@gmail.com>
2022-11-25 15:48:19 +1100

    part07_03

--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -4,8 +4,9 @@ public class MainProgram {
     public static void main(String[] args) {
         // write your test code here
         int[] array = {3, 1, 5, 99, 3, 12};
+        int[] numbers = {8, 3, 7, 9, 1, 2, 4};
+        MainProgram.sort(numbers);
     }
-<<<<<<< HEAD
     
     public static int smallest(int[]  array){
         int smallest = array[0];
@@ -31,7 +32,32 @@ public class MainProgram {
         return index;
     }
     
-=======
->>>>>>> 9a3dda1c293fb44dbbc9e0d0455a0f6edf25151e
+    public static int indexOfSmallestFrom(int[] table, int startIndex){
+        int index = startIndex;
+        int smallest = table[index];
+        
+            for(int i = startIndex; i < table.length; i++){
+            if(table[i] < smallest){
+                smallest = table[i];
+                index = i;
+            }
+        }
+            return index;
+    }
+    
+    public static void swap(int[] array, int index1, int index2){
+        int helper = array[index1];
+        array[index1] = array[index2];
+        array[index2] = helper;
+    }
+    
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int indexOfSmallest = indexOfSmallestFrom(array, i);
+            swap(array, i, indexOfSmallest);
+        }
+    }
+    
+    
 
 }
