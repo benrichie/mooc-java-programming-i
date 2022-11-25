68c13b2950725d2fabd759829d9b8d772bd69ff1
Benrichie Hewson <um2341583@gmail.com>
2022-11-25 13:11:35 +1100

    partial07_03

---
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -0,0 +1,34 @@
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        // write your test code here
+        int[] array = {3, 1, 5, 99, 3, 12};
+    }
+    
+    public static int smallest(int[]  array){
+        int smallest = array[0];
+        
+        for(int i = 0; i < array.length; i++){
+            if(array[i] < smallest){
+                smallest = array[i];
+            }
+        }
+        return smallest;
+    }
+    
+    public static int indexOfSmallest(int[] array){
+        int smallest = array[0];
+        int index = 0;
+        
+        for(int i = 0; i < array.length; i++){
+            if(array[i] < smallest){
+                smallest = array[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+    
+
+}
