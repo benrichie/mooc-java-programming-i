c8176da01801119b9cf1b065319e5dfb100fd47f
Benrichie Hewson <um2341583@gmail.com>
2022-12-01 18:45:47 +1100

    test

--- part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -1,10 +1,39 @@
 
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
         // insert test code here
+        // create the lists and arrays
+        
+        // array of integers 
         int[] array = {3, 1, 5, 99, 3, 12};
+        
+        // array of strings
+        String[] stringArray = {"one", "two", "three", "four"};
+        
+        // Arraylist of integers 
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(2);
+        numbers.add(9);
+        numbers.add(3);
+        numbers.add(6);
+        numbers.add(5);
+        numbers.add(4);
+        
+        //Arraylist of string 
+        ArrayList<String> string = new ArrayList<>();
+        string.add("x");
+        string.add("h");
+        string.add("f");
+        string.add("g");
+        string.add("d");
+        string.add("c");
+        
+        //use the methods
+        System.out.println("Array of in");
+        
     }
 
 }
