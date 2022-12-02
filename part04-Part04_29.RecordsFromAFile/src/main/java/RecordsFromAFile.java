d7961ab0c2c043160ab8a984a52103bf8f0cccf6
Benrichie Hewson <um2341583@gmail.com>
2022-12-02 15:24:31 +1100

    RecordsFromAFile

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -1,11 +1,29 @@
-
 import java.nio.file.Paths;
 import java.util.Scanner;
-
+ 
 public class RecordsFromAFile {
-
+ 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String input = scanner.nextLine();
+        
+        try(Scanner fileReader = new Scanner(Paths.get(input))){
+            while(fileReader.hasNextLine()){
+                String[] line = fileReader.nextLine().split(",");
+                
+                System.out.println(line[0]+", age: "+ line[1]+ " years");
+            
+                
+                
+                if (input.isEmpty()){
+                    break;
+                }
+                
+            }
+            
+            } catch (Exception e){
+                    System.out.println("Error"+ e.getMessage());
+                    
+        }
     }
 }
